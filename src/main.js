4ca40e517bed307e103dd2169a2ee4f504b8fc3f
Henry Xiang <hxxiang@ucdavis.edu>
2017-02-13 10:08:10 -0800

    fixed import error using rx-dom

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
-import Rx from 'rxjs/Rx';
+import Rx from 'rx-dom';
 
 console.log("App started");
 
+Rx.DOM.ready()
+  .subscribe(v => console.log("DOM ready"));
+
 Rx.Observable.of(1,2,3)
   .subscribe(v => console.log(v));
+
 
 $("#head").html("It works!");
 
